2b0ecba9abdb8a0247e2d8d6d4ade645f25baf7c
ishitasinghvi2006-sys <ishita.singhvi2006@gmail.com>
2026-05-20 14:20:30 +0530

    fix prisma client

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,6 @@ import 'dotenv/config'
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
     url: process.env.DATABASE_URL!,
   },
