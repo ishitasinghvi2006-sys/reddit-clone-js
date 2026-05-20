c948b9c480a949e28cd4e8f6ce10a4337839623a
ishitasinghvi2006-sys <ishita.singhvi2006@gmail.com>
2026-05-20 18:44:07 +0530

    fix prisma 7 client

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -2,13 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-})
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
